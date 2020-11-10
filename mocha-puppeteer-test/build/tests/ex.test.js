42feb9f50daae347fd94ca7adf9f9b1612be222e
mazdak <mazdak@localhost.localdomain>
2020-11-10 06:23:28 +0100

    mocha puppeteer done

--- mocha-puppeteer-test/build/tests/ex.test.js
+++ mocha-puppeteer-test/build/tests/ex.test.js
@@ -8,15 +8,21 @@ var _builder = require('../builder');
 
 var _builder2 = _interopRequireDefault(_builder);
 
+var _LoginPage = require('../pages/LoginPage');
+
+var _LoginPage2 = _interopRequireDefault(_LoginPage);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 describe('Name of the group', function () {
 	var page = void 0;
+	var loginPage = void 0;
 	// let mobile
 
 	before(async function () {
 		page = await _builder2.default.build('Desktop');
 		// mobile = await Page.build('Mobile');
+		loginPage = new _LoginPage2.default(page);
 	});
 
 	after(async function () {
@@ -26,23 +32,24 @@ describe('Name of the group', function () {
 	(0, _mochaSteps.step)('should load home page', async function () {
 		await page.goto('http://zero.webappsecurity.com/index.html');
 		await page.waitAndClick('#onlineBankingMenu');
-		var signInBtn = await page.isElementVisible('#signin_button');
-		(0, _chai.expect)(signInBtn).to.be.true;
+		(0, _chai.expect)((await page.isElementVisible('#signin_button'))).to.be.true;
 	});
 
 	(0, _mochaSteps.step)('should display login page', async function () {
 		await page.waitAndClick('#signin_button');
-		var loginForm = await page.isElementVisible('#login_form');
-		(0, _chai.expect)(loginForm).to.be.true;
-		var signInBtn = await page.isElementVisible('#signin_button');
-		(0, _chai.expect)(signInBtn).to.be.false;
+		(0, _chai.expect)((await page.isElementVisible('#login_form'))).to.be.true;
+		(0, _chai.expect)((await page.isElementVisible('#signin_button'))).to.be.false;
 	});
 
 	(0, _mochaSteps.step)('should login to app', async function () {
-		await page.waitAndType('#user_login', 'username');
-		await page.waitAndType('#user_password', 'password');
-		await page.waitAndClick('.btn-primary');
-		var navbar = await page.isElementVisible('.nav-tabs');
-		(0, _chai.expect)(navbar).to.be.true;
+		// await page.waitAndType('#user_login', 'username');
+		// await page.waitAndType('#user_password', 'password');
+		// await page.waitAndClick('.btn-primary');
+		await loginPage.login('username', 'password');
+		(0, _chai.expect)((await page.isElementVisible('.nav-tabs'))).to.be.true;
+	});
+
+	(0, _mochaSteps.step)('should have 6 navbar links', async function () {
+		(0, _chai.expect)((await page.getCount('.nav-tabs li'))).to.be.equal(6);
 	});
 });
--- mocha-puppeteer-test/src/tests/ex.test.js
+++ mocha-puppeteer-test/src/tests/ex.test.js
@@ -1,14 +1,17 @@
 import { step } from 'mocha-steps';
 import { expect } from 'chai';
 import Page from '../builder';
+import LoginPage from '../pages/LoginPage';
 
 describe('Name of the group', () => {
 	let page;
+	let loginPage;
 	// let mobile
 
 	before(async () => {
 		page = await Page.build('Desktop');
 		// mobile = await Page.build('Mobile');
+		loginPage = new LoginPage(page);
 	});
 
 	after(async () => {
@@ -18,23 +21,24 @@ describe('Name of the group', () => {
 	step('should load home page', async () => {
 		await page.goto('http://zero.webappsecurity.com/index.html');
 		await page.waitAndClick('#onlineBankingMenu');
-		const signInBtn = await page.isElementVisible('#signin_button');
-		expect(signInBtn).to.be.true;
+		expect(await page.isElementVisible('#signin_button')).to.be.true;
 	});
 
 	step('should display login page', async () => {
 		await page.waitAndClick('#signin_button');
-		const loginForm = await page.isElementVisible('#login_form');
-		expect(loginForm).to.be.true;
-		const signInBtn = await page.isElementVisible('#signin_button');
-		expect(signInBtn).to.be.false;
+		expect(await page.isElementVisible('#login_form')).to.be.true;
+		expect(await page.isElementVisible('#signin_button')).to.be.false;
 	});
 
 	step('should login to app', async () => {
-		await page.waitAndType('#user_login', 'username');
-		await page.waitAndType('#user_password', 'password');
-		await page.waitAndClick('.btn-primary');
-		const navbar = await page.isElementVisible('.nav-tabs');
-		expect(navbar).to.be.true;
+		// await page.waitAndType('#user_login', 'username');
+		// await page.waitAndType('#user_password', 'password');
+		// await page.waitAndClick('.btn-primary');
+		await loginPage.login('username', 'password');
+		expect(await page.isElementVisible('.nav-tabs')).to.be.true;
+	});
+
+	step('should have 6 navbar links', async () => {
+		expect(await page.getCount('.nav-tabs li')).to.be.equal(6);
 	});
 });
